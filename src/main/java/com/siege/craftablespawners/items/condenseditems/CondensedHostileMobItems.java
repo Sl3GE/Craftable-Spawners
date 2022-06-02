22307d1e7b57c36549b5ec6a6b29c413f6284079
Sl3GE <siegecaster@gmail.com>
2022-06-01 22:55:30 -0400

    added branch changes back

--- src/main/java/com/siege/craftablespawners/items/condenseditems/CondensedHostileMobItems.java
+++ src/main/java/com/siege/craftablespawners/items/condenseditems/CondensedHostileMobItems.java
@@ -20,122 +20,25 @@ public class CondensedHostileMobItems {
     public static ItemStack condensedBlockOfRedstone;
 
     public static void init() {
-        createCondensedBone();
-        createCondensedRottenFlesh();
-        createCondensedBlazeRod();
-        createCondensedGunpowder();
-        createCondensedString();
-        createCondensedEnderPearl();
-        createCondensedSlimeBlock();
-        createCondensedMagmaCream();
-        createCondensedGhastTear();
-        createCondensedGoldBlock();
-        createCondensedNetherStar();
-        createCondensedWitherSkeletonSkull();
-        createCondensedBlockOfRedstone();
+        createCondensedItem(Material.BONE,"§2Condensed Bone", "condensed_bone");
+        createCondensedItem(Material.ROTTEN_FLESH,"§2Condensed Rotten Flesh","condensed_rotten_flesh");
+        createCondensedItem(Material.BLAZE_ROD,"§2Condensed Blaze Rod","condensed_blaze_rod");
+        createCondensedItem(Material.GUNPOWDER,"§2Condensed Gunpowder", "condensed_gunpowder");
+        createCondensedItem(Material.STRING,"§2Condensed String", "condensed_string");
+        createCondensedItem(Material.ENDER_PEARL,"§2Condensed Ender Pearl", "condensed_ender_pearl");
+        createCondensedItem(Material.SLIME_BLOCK,"§2Condensed Slime Block", "condensed_slime_block");
+        createCondensedItem(Material.MAGMA_CREAM,"§2Condensed Magma Cream", "condensed_magma_cream");
+        createCondensedItem(Material.GHAST_TEAR,"§2Condensed Ghast Tear", "condensed_ghast_tear");
+        createCondensedItem(Material.GOLD_BLOCK,"§2Condensed Gold Block", "condensed_gold_block");
+        createCondensedItem(Material.NETHER_STAR,"§2Condensed Nether Star", "condensed_nether_star");
+        createCondensedItem(Material.WITHER_SKELETON_SKULL,"§2Condensed Wither Skeleton Skull", "condensed_wither_skeleton_skull");
+        createCondensedItem(Material.REDSTONE_BLOCK,"§2Condensed Redstone Block", "condensed_redstone_block");
     }
 
-    private static void createCondensedBone() {
-        condensedBone = AbstractedCreationMethods.createItem(Material.BONE, 1, "§2Condensed Bone");
-
-        AbstractedCreationMethods.condenseItems(condensedBone,"condensed_bone", new ItemStack(Material.BONE,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.BONE,9),"revert_condensed_bone", condensedBone);
+    private static void createCondensedItem(Material material, String name, String recipeKey) {
+        ItemStack item = AbstractedCreationMethods.createItem(material, 1, name);
+        AbstractedCreationMethods.condenseItems(item,recipeKey, new ItemStack(material,1));
+        AbstractedCreationMethods.separateCondensedItem(new ItemStack(material,9),"revert_"+recipeKey, item);
     }
 
-    private static void createCondensedRottenFlesh() {
-        condensedRottenFlesh = AbstractedCreationMethods.createItem(Material.ROTTEN_FLESH, 1, "§2Condensed Rotten Flesh");
-
-        AbstractedCreationMethods.condenseItems(condensedRottenFlesh,"condensed_rotten_flesh", new ItemStack(Material.ROTTEN_FLESH,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.ROTTEN_FLESH,9),"revert_condensed_rotten_flesh", condensedRottenFlesh);
-    }
-
-    private static void createCondensedBlazeRod() {
-        condensedBlazeRod = AbstractedCreationMethods.createItem(Material.BLAZE_ROD, 1, "§2Condensed Blaze Rod");
-
-        AbstractedCreationMethods.condenseItems(condensedBlazeRod,"condensed_blaze_rod", new ItemStack(Material.BLAZE_ROD,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.BLAZE_ROD,9),"revert_condensed_blaze_rod", condensedBlazeRod);
-    }
-
-    private static void createCondensedGunpowder() {
-        condensedGunpowder = AbstractedCreationMethods.createItem(Material.GUNPOWDER, 1, "§2Condensed Gunpowder");
-
-        AbstractedCreationMethods.condenseItems(condensedGunpowder,"condensed_gunpowder", new ItemStack(Material.GUNPOWDER,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.GUNPOWDER,9),"revert_condensed_gunpowder", condensedGunpowder);
-    }
-
-    private static void createCondensedString() {
-        condensedString = AbstractedCreationMethods.createItem(Material.STRING, 1, "§2Condensed String");
-
-        AbstractedCreationMethods.condenseItems(condensedString,"condensed_string", new ItemStack(Material.STRING,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.STRING,9),"revert_condensed_string", condensedString);
-    }
-
-    private static void createCondensedEnderPearl() {
-        condensedEnderPearl = AbstractedCreationMethods.createItem(Material.ENDER_PEARL, 1, "§2Condensed Ender Pearl");
-
-        AbstractedCreationMethods.condenseItems(condensedEnderPearl,"condensed_ender_pearl", new ItemStack(Material.ENDER_PEARL,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.ENDER_PEARL,9),"revert_condensed_ender_pearl", condensedEnderPearl);
-    }
-
-    private static void createCondensedSlimeBlock() {
-        condensedSlimeBlock = AbstractedCreationMethods.createItem(Material.SLIME_BLOCK, 1, "§2Condensed Slime Block");
-
-        AbstractedCreationMethods.condenseItems(condensedSlimeBlock,"condensed_slime_block", new ItemStack(Material.SLIME_BLOCK,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.SLIME_BLOCK,9),"revert_condensed_slime_block", condensedSlimeBlock);
-    }
-
-    private static void createCondensedMagmaCream() {
-        condensedMagmaCream = AbstractedCreationMethods.createItem(Material.MAGMA_CREAM, 1, "§2Condensed Magma Cream");
-
-        AbstractedCreationMethods.condenseItems(condensedMagmaCream,"condensed_magma_cream", new ItemStack(Material.MAGMA_CREAM,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.MAGMA_CREAM,9),"revert_condensed_magma_cream", condensedMagmaCream);
-    }
-
-    private static void createCondensedGhastTear() {
-        condensedGhastTear = AbstractedCreationMethods.createItem(Material.GHAST_TEAR, 1, "§2Condensed Ghast Tear");
-
-        AbstractedCreationMethods.condenseItems(condensedGhastTear,"condensed_ghast_tear", new ItemStack(Material.GHAST_TEAR,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.GHAST_TEAR,9),"revert_condensed_ghast_tear", condensedGhastTear);
-    }
-
-    private static void createCondensedGoldBlock() {
-        condensedGoldBlock = AbstractedCreationMethods.createItem(Material.GOLD_BLOCK, 1, "§2Condensed Gold Block");
-
-        AbstractedCreationMethods.condenseItems(condensedGoldBlock,"condensed_gold_block", new ItemStack(Material.GOLD_BLOCK,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.GOLD_BLOCK,9),"revert_condensed_gold_block", condensedGoldBlock);
-    }
-
-    private static void createCondensedNetherStar() {
-        condensedNetherStar = AbstractedCreationMethods.createItem(Material.NETHER_STAR, 1, "§2Condensed Nether Star");
-
-        AbstractedCreationMethods.condenseItems(condensedNetherStar,"condensed_nether_star", new ItemStack(Material.NETHER_STAR,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.NETHER_STAR,9),"revert_condensed_nether_star", condensedNetherStar);
-    }
-
-    private static void createCondensedWitherSkeletonSkull() {
-        condensedWitherSkeletonSkull = AbstractedCreationMethods.createItem(Material.WITHER_SKELETON_SKULL, 1, "§2Condensed Wither Skeleton Skull");
-
-        AbstractedCreationMethods.condenseItems(condensedWitherSkeletonSkull,"condensed_wither_skeleton_skull", new ItemStack(Material.WITHER_SKELETON_SKULL,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.WITHER_SKELETON_SKULL,9),"revert_condensed_wither_skeleton_skull", condensedWitherSkeletonSkull);
-    }
-
-    private static void createCondensedBlockOfRedstone() {
-        condensedBlockOfRedstone = AbstractedCreationMethods.createItem(Material.REDSTONE_BLOCK, 1, "§2Condensed Block Of Redstone");
-
-        AbstractedCreationMethods.condenseItems(condensedBlockOfRedstone,"condensed_block_of_redstone", new ItemStack(Material.REDSTONE_BLOCK,1));
-
-        AbstractedCreationMethods.separateCondensedItem(new ItemStack(Material.REDSTONE_BLOCK,9),"revert_condensed_block_of_redstone", condensedBlockOfRedstone);
-    }
 }
